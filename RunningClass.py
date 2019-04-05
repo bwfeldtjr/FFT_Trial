ae5e60b8e468209772949f07895d21038d57fdda
bwfeldtjr <47539251+bwfeldtjr@users.noreply.github.com>
2019-04-05 14:33:45 -0500

    Add files via upload

--- RunningClass.py
+++ RunningClass.py
@@ -13,12 +13,20 @@ import KM_Class as KM
 #from mpl_toolkits import mplot3d
 
 
-filename = 'PSI_TSERIES.csv' #input("Input file (include extension but not apostraphes) = ")
+filename = 'PSI_TSERIES.csv' #input("Input file (include extension) = ")
 dt = 0.1 #input("dt for Taylor Function = ")
+regrnum = 3 #input("regrnum for KM_Class")
+list2 = np.arange(0,100)
 
 
-y = PSD.PSD_Class()
-y.Taylor(dt,filename)
+dec1 = 'y' #input("Run PSD_Class (y/n) ")
+dec2 = 'y' #input("Run KM_Class (y/n) ")
 
 x = KM.KM_Class()
-x.Everything_else(filename,dt)
+y = PSD.PSD_Class()
+
+if dec1 == 'y':
+    y.Taylor(float(dt),filename)
+
+if dec2 == 'y':
+    x.Everything_else(filename,float(dt),int(regrnum), list2)
